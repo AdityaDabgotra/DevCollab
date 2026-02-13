16c143d66b3b5b4ae871feeb5aa23bb4de848e70
AdityaDabgotra <adityadabgotra2004@gmail.com>
2026-02-13 15:59:47 +0530

    sign route created

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,72 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import dbConnect from "@/lib/db";
+import UserModel from "@/models/User";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        identifier: {
+          label: "Email or Username",
+          type: "text",
+          placeholder: "Enter your email or username",
+        },
+        password: { label: "Password", type: "password" },
+      },
+
+      async authorize(credentials: any): Promise<any> {
+        await dbConnect();
+
+        const user = await UserModel.findOne({
+          $or: [
+            { email: credentials.identifier },
+            { userName: credentials.identifier },
+          ],
+        });
+
+        if (!user) {
+          throw new Error("No user found with the given credentials");
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(
+          credentials.password,
+          user.password
+        );
+
+        if (!isPasswordCorrect) {
+          throw new Error("Incorrect password");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user._id?.toString();
+        token.username = (user as any).userName;
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user._id = token._id?.toString();
+        session.user.username = token.username;
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
